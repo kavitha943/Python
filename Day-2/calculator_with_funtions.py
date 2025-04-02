2d684a541e34da97b2936997f10479828eb86b1c
Kavitha Gudibandi <166821151+kavitha943@users.noreply.github.com>
2025-04-02 15:26:35 +0000

    add files for day-02

---
+++ Day-2/calculator_with_funtions.py
@@ -0,0 +1,17 @@
+def addition(a,b):
+    result = a + b
+    return result
+def sub(a,b):
+    res = a-b
+    return res
+def mul(a,b):
+    result = a*b
+    return result
+
+# these lines are not needed when you want to use this file as module , if you do so it will call all the
+# funtions and prints it outputs eventhough if you call  one function or 
+#remove the print and then use it, then you get required funtion output.
+
+#print(addition(5,2))
+#print(sub(5,2)) 
+#print(mul(5,2))
---
+++ Day-2/calculator_with_module.py
@@ -0,0 +1,9 @@
+from calculator_with_funtions import *
+
+def div(a,b):
+    return a//b
+
+print(addition(22,4))
+print(sub(2,3))
+print(mul(3,4))
+print(div(2,3))
